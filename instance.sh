d6c37b8263a9b279e3da5666f789ba020569ae3e
ChittamuruMonika <chittamurumonika2001@gmail.com>
2025-10-04 22:50:45 +0530

    roboshop

--- instance.sh
+++ instance.sh
@@ -5,7 +5,7 @@ sg_id="sg-047d12ce1fc8acf06"
 
 for instance in $@
 do 
-    instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text)
+    instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
@@ -13,4 +13,4 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
     echo "$instance : $IP"
-then
+done
